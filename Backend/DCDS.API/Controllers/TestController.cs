3d065cdab8fb5ed38d5f7a1d39d40bd21cd9ce54
NicolasLepore <nicolaslpr06@gmail.com>
2025-10-04 11:09:41 -0300

    Test endpoint

---
+++ Backend/DCDS.API/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DCDS.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/test")]
+    public class TestController : ControllerBase
+    {
+
+        public TestController()
+        {
+        }
+
+        [HttpGet]
+        public ActionResult GetTest()
+        {
+            return Ok(new { Message = "Hello World", Status = StatusCodes.Status200OK });
+        }
+    }
+}
