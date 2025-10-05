50121385ed9c65c6e050d5ae6cc4ad7d27f0e9d6
Alberto Araujo <albertoaraujo.dev@gmail.com>
2025-10-05 16:00:04 -0300

    feat: add calendar module for desktop/mobile and UI components from shadcn, update routing and styles.

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./modules/home";
+import Calendar from "./modules/calendar";
 import Login from "./modules/login";
 import Layout from "./componentes/layout";
  
@@ -11,6 +12,7 @@ function App() {
         <Layout> 
           <Routes>
             <Route element={<Home />} path={"/"} />
+            <Route element={<Calendar />} path={"/calendar"} />
             <Route element={<Login />} path={"/login"} />
           </Routes>
         </Layout>
